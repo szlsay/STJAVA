5f735c7e1469998ac8aced26886d7a516772743d
沈天 <409178030@qq.com>
2021-08-31 17:47:54 +0800

    rabbitmq

---
+++ 12-RabbitMQ/rabbitmqtest/src/main/java/cn/st/rabbitmq/simple/Producer.java
@@ -0,0 +1,56 @@
+package cn.st.rabbitmq.simple;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Producer {
+
+    static final String QUEUE_NAME = "simple_queue";
+
+    public static void main(String[] args) throws Exception {
+        //创建连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        //主机地址;默认为 localhost
+        connectionFactory.setHost("localhost");
+        //连接端口;默认为 5672
+        connectionFactory.setPort(5672);
+        //虚拟主机名称;默认为 /
+        connectionFactory.setVirtualHost("/stvh");
+        //连接用户名；默认为guest
+        connectionFactory.setUsername("st00");
+        //连接密码；默认为guest
+        connectionFactory.setPassword("123456");
+
+        //创建连接
+        Connection connection = connectionFactory.newConnection();
+
+        // 创建频道
+        Channel channel = connection.createChannel();
+
+        // 声明（创建）队列
+        /**
+         * 参数1：队列名称
+         * 参数2：是否定义持久化队列
+         * 参数3：是否独占本次连接
+         * 参数4：是否在不使用的时候自动删除队列
+         * 参数5：队列其它参数
+         */
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+
+        // 要发送的信息
+        String message = "你好；小兔子！";
+        /**
+         * 参数1：交换机名称，如果没有指定则使用默认Default Exchage
+         * 参数2：路由key,简单模式可以传递队列名称
+         * 参数3：消息其它属性
+         * 参数4：消息内容
+         */
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println("已发送消息：" + message);
+
+        // 关闭资源
+        channel.close();
+        connection.close();
+    }
+}
---
+++ 12-RabbitMQ/rabbitmqtest/src/main/java/cn/st/rabbitmq/util/ConnectionUtil.java
@@ -0,0 +1,25 @@
+package cn.st.rabbitmq.util;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class ConnectionUtil {
+
+    public static Connection getConnection() throws Exception {
+        //创建连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        //主机地址;默认为 localhost
+        connectionFactory.setHost("localhost");
+        //连接端口;默认为 5672
+        connectionFactory.setPort(5672);
+        //虚拟主机名称;默认为 /
+        connectionFactory.setVirtualHost("/stvh");
+        //连接用户名；默认为guest
+        connectionFactory.setUsername("st00");
+        //连接密码；默认为guest
+        connectionFactory.setPassword("123456");
+
+        //创建连接
+        return connectionFactory.newConnection();
+    }
+}
