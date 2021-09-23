814c42fbde7dc7a2fbbdefb72249ada9105043b8
沈天 <STShenZhaoliang@users.noreply.github.com>
2021-09-23 17:14:56 +0800

    javase-02

---
+++ 01-JavaSE/day02-运算符&分支语句/day02/src/运算符04/自增自减运算符/OperatorDemo.java
@@ -0,0 +1,22 @@
+package 运算符04.自增自减运算符;
+/*
+	自增自减运算符
+*/
+public class OperatorDemo {
+	public static void main(String[] args) {
+		//定义变量
+		int i = 10;
+		System.out.println("i:" + i);
+
+		//单独使用
+		//i++;
+		//++i;
+		//System.out.println("i:" + i);
+
+		//参与操作使用
+		//int j = i++;
+		int j = ++i;
+		System.out.println("i:" + i);
+		System.out.println("j:" + j);
+	}
+}
