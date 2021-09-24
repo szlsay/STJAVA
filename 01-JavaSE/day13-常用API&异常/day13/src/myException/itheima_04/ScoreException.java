6cab261204e7bd8c3999046339f27872eb00ebed
沈天 <13865738+szlsay@users.noreply.github.com>
2021-09-24 17:09:18 +0800

    javase-13

---
+++ 01-JavaSE/day13-常用API&异常/day13/src/myException/itheima_04/ScoreException.java
@@ -0,0 +1,11 @@
+package myException.itheima_04;
+
+public class ScoreException extends Exception {
+
+    public ScoreException() {}
+
+    public ScoreException(String message) {
+        super(message);
+    }
+
+}
