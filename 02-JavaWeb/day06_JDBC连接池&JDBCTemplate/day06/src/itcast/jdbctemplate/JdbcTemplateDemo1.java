38e59b4178c6e00816ee0c9b111d8f8b900085c4
沈天 <13865738+szlsay@users.noreply.github.com>
2021-09-27 11:25:27 +0800

    javaweb-06

---
+++ 02-JavaWeb/day06_JDBC连接池&JDBCTemplate/day06/src/itcast/jdbctemplate/JdbcTemplateDemo1.java
@@ -0,0 +1,20 @@
+package itcast.jdbctemplate;
+
+import itcast.utils.JDBCUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * JdbcTemplate入门
+ */
+public class JdbcTemplateDemo1 {
+
+    public static void main(String[] args) {
+        //1.导入jar包
+        //2.创建JDBCTemplate对象
+        JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
+        //3.调用方法
+        String sql = "update account set balance = 5000 where id = ?";
+        int count = template.update(sql, 4);
+        System.out.println(count);
+    }
+}
