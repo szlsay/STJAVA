b28639f74ba188cbcdf9f9cd71a2ef55d6f96190
szlsay <13865738+szlsay@users.noreply.github.com>
2021-10-09 17:37:33 +0800

    spring-jdbc

---
+++ 03-Spring/day04_JdbcTemplate基本使用/spring_jdbc/src/test/java/com/itheima/domain/Account.java
@@ -0,0 +1,31 @@
+package com.itheima.domain;
+
+public class Account {
+
+    private String name;
+    private int balance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "name='" + name + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
