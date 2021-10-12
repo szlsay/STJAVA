0781f69c22ebc21937533d769823e21785581465
szlsay <13865738+szlsay@users.noreply.github.com>
2021-10-12 15:51:48 +0800

    maven-demo

---
+++ 07-Maven/maven_demo/src/main/java/com/itheima/MavenTest2.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+import org.junit.Test;
+
+public class MavenTest2 {
+    @Test
+    public void test2(){
+        System.out.println("test2()...");
+    }
+}
---
+++ 07-Maven/maven_demo/src/test/java/com/itheima/MavenTest.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+import org.junit.Test;
+
+public class MavenTest {
+    @Test
+    public void test1(){
+        System.out.println("test()...");
+    }
+}
