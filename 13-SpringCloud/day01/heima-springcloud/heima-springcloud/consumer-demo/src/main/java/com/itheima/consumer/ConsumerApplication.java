bcaf2d07bd4b6cd12fe6480c85c84f89b037a2ce
szlsay <13865738+szlsay@users.noreply.github.com>
2021-10-15 17:30:52 +0800

    springcloud-eureka

--- 13-SpringCloud/day01/heima-springcloud/heima-springcloud/consumer-demo/src/main/java/com/itheima/consumer/ConsumerApplication.java
+++ 13-SpringCloud/day01/heima-springcloud/heima-springcloud/consumer-demo/src/main/java/com/itheima/consumer/ConsumerApplication.java
@@ -1,25 +1,23 @@
 package com.itheima.consumer;
 
 import org.springframework.boot.SpringApplication;
-//import org.springframework.cloud.client.SpringCloudApplication;
-//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-/*@SpringBootApplication
-@EnableDiscoveryClient //开启Eureka客户端发现功能
-@EnableCircuitBreaker //开启熔断*/
-//@SpringCloudApplication
+//@SpringBootApplication
+//@EnableDiscoveryClient //开启Eureka客户端发现功能
+//@EnableCircuitBreaker //开启熔断*/
 
-@SpringBootApplication
+@SpringCloudApplication  // 相对于上面三个
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
     @Bean
-//    @LoadBalanced
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
--- 13-SpringCloud/day01/heima-springcloud/heima-springcloud/consumer-demo/src/main/java/com/itheima/consumer/controller/ConsumerController.java
+++ 13-SpringCloud/day01/heima-springcloud/heima-springcloud/consumer-demo/src/main/java/com/itheima/consumer/controller/ConsumerController.java
@@ -5,13 +5,19 @@ package com.itheima.consumer.controller;
 //import org.springframework.cloud.client.discovery.DiscoveryClient;
 //import org.springframework.web.bind.annotation.PathVariable;
 
+import com.itheima.consumer.pojo.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/consumer")
@@ -22,9 +28,30 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/{id}")
+    @HystrixCommand(fallbackMethod = "queryByIdFallback")
     public String queryById(@PathVariable Long id){
-        String url = "http://localhost:9091/user/"+id;
+
+        if (id == 1) {
+            throw new RuntimeException("太忙了");
+        }
+
+        // 方法1
+//        String url = "http://localhost:9091/user/"+id;
+//        return restTemplate.getForObject(url, String.class);
+
+        // 方法2
+        //获取eureka中注册的user-service的实例
+//        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("user-service");
+//        ServiceInstance serviceInstance = serviceInstances.get(0);
+//        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + id;
+//        return restTemplate.getForObject(url, String.class);
+
+        // 方法3
+        String url = "http://user-service/user/" + id;
         return restTemplate.getForObject(url, String.class);
     }
 
