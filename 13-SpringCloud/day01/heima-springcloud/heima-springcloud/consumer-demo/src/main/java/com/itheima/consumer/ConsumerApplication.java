a42ce788405d545cdab20f8d6f291d352d8af4ff
szlsay <13865738+szlsay@users.noreply.github.com>
2021-10-15 14:25:07 +0800

    springcloud-consumer

---
+++ 13-SpringCloud/day01/heima-springcloud/heima-springcloud/consumer-demo/src/main/java/com/itheima/consumer/ConsumerApplication.java
@@ -0,0 +1,26 @@
+package com.itheima.consumer;
+
+import org.springframework.boot.SpringApplication;
+//import org.springframework.cloud.client.SpringCloudApplication;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/*@SpringBootApplication
+@EnableDiscoveryClient //开启Eureka客户端发现功能
+@EnableCircuitBreaker //开启熔断*/
+//@SpringCloudApplication
+
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @Bean
+//    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
